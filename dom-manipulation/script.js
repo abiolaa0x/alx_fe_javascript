4451f801554a68e2850f7aab88b22b98a72c2978
JayCodes <www.amujonathan@gmail.com>
2025-12-21 21:48:01 +0100

    some fixx.

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -153,7 +153,7 @@ async function syncQuotes() {
     populateCategories();
 
     syncStatus.textContent =
-      "Quotes synced with server.";
+      "Quotes synced with server!";
 
     await postQuotesToServer(quotes);
   } catch (error) {
